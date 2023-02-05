0f65f6b2852b891eccf0b5e30c951aab94f052f9
kmpfwgn <ilya.gayduk.99@gmail.com>
2023-02-05 12:25:23 +0100

    add custom prometheus config

--- monitoring/prometheus/run-docker.ps1
+++ monitoring/prometheus/run-docker.ps1
@@ -1,1 +1,1 @@
-docker run -d --name prometheus -p 9090:9090 prom/prometheus
+docker run -d --name prometheus -p 9090:9090 -v ./prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
--- monitoring/prometheus/run-docker.sh
+++ monitoring/prometheus/run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name prometheus -p 9090:9090 prom/prometheus
+docker run -d --name prometheus -p 9090:9090 -v ./prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
