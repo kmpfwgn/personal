fafe3c509c6eb96adab89ee1c1763e32f58d99d5
kmpfwgn <ilya.gayduk.99@gmail.com>
2023-02-05 12:49:03 +0100

    add prometheus local storage

--- monitoring/prometheus/run-docker.ps1
+++ monitoring/prometheus/run-docker.ps1
@@ -1,1 +1,1 @@
-docker run -d --name prometheus -p 9090:9090 -v ${pwd}/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run -d --name prometheus -p 9090:9090 -v ${pwd}/prometheus.yml:/etc/prometheus/prometheus.yml -v F:/storage/prometheus:/prometheus prom/prometheus
